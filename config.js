d68137eb250e19483d903a2de4fd9b3a4eb97e0a
hawaryou <hawaryougaming@gmail.com>
2026-09-18 17:18:30 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Configuration de la fiche ENR08-V05
+// URL Google Apps Script déjà configurée.
+window.GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwXoa9jjmye4AMvUFHiK_2wChRKWF-yA0qqWSLTHQJYY9Vr1h3NddRwkLQe8nwUvkH5/exec";
